d5bbb2e48c7ba4f32ac76d1201d9d29af470d987
SaberCon <894876785@qq.com>
2021-08-18 10:02:29 +0800

    :art: improve

--- src/main/kotlin/cn/sabercon/mogumin/base/Models.kt
+++ src/main/kotlin/cn/sabercon/mogumin/base/Models.kt
@@ -1,6 +1,6 @@
 package cn.sabercon.mogumin.base
 
-data class Result<T : Any>(
+data class Result<out T : Any>(
     val ok: Boolean = true,
     val code: String? = null,
     val msg: String? = null,
@@ -8,18 +8,16 @@ data class Result<T : Any>(
     val data: T? = null,
 )
 
-@Suppress("UNCHECKED_CAST")
-fun <T : Any> emptyResult(): Result<T> = EMPTY_RESULT as Result<T>
+fun <T : Any> emptyResult(): Result<T> = EMPTY_RESULT
 
 val EMPTY_RESULT = Result<Nothing>()
 
-data class Page<T : Any>(
+data class Page<out T : Any>(
     val total: Long,
     val list: List<T>,
 )
 
-@Suppress("UNCHECKED_CAST")
-fun <T : Any> emptyPage(): Page<T> = EMPTY_PAGE as Page<T>
+fun <T : Any> emptyPage(): Page<T> = EMPTY_PAGE
 
 val EMPTY_PAGE = Page<Nothing>(0, emptyList())
 
