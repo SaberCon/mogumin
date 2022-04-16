2a63af75119a98a79fd4600316cd322064578e9a
SaberCon <894876785@qq.com>
2022-04-16 20:36:29 +0800

    :arrow_up: upgrade to spring boot 2.7.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
-    id("org.springframework.boot") version "2.6.1"
+    id("org.springframework.boot") version "2.7.0-SNAPSHOT"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.6.0"
-    kotlin("plugin.spring") version "1.6.0"
-    id("com.google.devtools.ksp") version "1.6.0-1.0.2"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10"
 }
 
 group = "cn.sabercon"
@@ -19,21 +18,15 @@ configurations {
     }
 }
 
-kotlin {
-    sourceSets.main {
-        kotlin.srcDir("build/generated/ksp/main/kotlin")
-    }
-    sourceSets.test {
-        kotlin.srcDir("build/generated/ksp/test/kotlin")
-    }
-}
-
 repositories {
     mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-graphql")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
@@ -43,21 +36,18 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.flywaydb:flyway-core")
 
-    implementation("com.google.guava:guava:31.0.1-jre")
+    implementation("com.google.guava:guava:31.1-jre")
     implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.4")
-    implementation("com.auth0:java-jwt:3.18.3")
+    implementation("com.auth0:java-jwt:3.19.1")
 
-    implementation(platform("io.arrow-kt:arrow-stack:1.0.1"))
-    implementation("io.arrow-kt:arrow-core")
-    implementation("io.arrow-kt:arrow-fx-coroutines")
-    implementation("io.arrow-kt:arrow-optics")
-    ksp("io.arrow-kt:arrow-optics-ksp-plugin:1.0.3-alpha.1")
-
-    runtimeOnly("io.r2dbc:r2dbc-postgresql")
+    runtimeOnly("io.r2dbc:r2dbc-postgresql:0.8.12.RELEASE")
+    runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.graphql:spring-graphql-test")
     testImplementation("io.projectreactor:reactor-test")
 }
 
--- src/main/kotlin/cn/sabercon/common/data/r2dbc/Config.kt
+++ src/main/kotlin/cn/sabercon/common/data/r2dbc/Config.kt
@@ -5,24 +5,35 @@ import cn.sabercon.common.data.ID
 import cn.sabercon.common.data.MTIME
 import cn.sabercon.common.util.UnsafeReflection
 import cn.sabercon.common.util.now
-import io.r2dbc.spi.ConnectionFactory
+import io.r2dbc.spi.ConnectionFactoryOptions
+import io.r2dbc.spi.Option
+import org.flywaydb.core.Flyway
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.ClassPathResource
 import org.springframework.data.r2dbc.mapping.event.BeforeConvertCallback
 import org.springframework.data.relational.core.mapping.NamingStrategy
 import org.springframework.data.relational.core.mapping.RelationalPersistentProperty
-import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 import reactor.kotlin.core.publisher.toMono
 
 @Configuration
 class R2dbcConfig {
 
-    @Bean
-    fun initializer(connectionFactory: ConnectionFactory) = ConnectionFactoryInitializer().apply {
-        setConnectionFactory(connectionFactory)
-        setDatabasePopulator(ResourceDatabasePopulator(ClassPathResource("schema.sql")))
+    @Bean(initMethod = "migrate")
+    fun flyway(props: R2dbcProperties): Flyway {
+        val options = ConnectionFactoryOptions.parse(props.url)
+        val driver = options.getValue(Option.valueOf<String>("driver")) as String
+        val host = options.getValue(Option.valueOf<String>("host")) as String
+        val port = options.getValue(Option.valueOf<Int>("port")) as Int
+        val database = options.getValue(Option.valueOf<String>("database")) as String
+        val user = options.getValue(Option.valueOf<String>("user")) as String
+        val password = options.getValue(Option.sensitiveValueOf<String>("password")) as String
+        val jdbcUrl = "jdbc:$driver://$host:$port/$database"
+
+        return Flyway.configure()
+            .baselineOnMigrate(true)
+            .dataSource(jdbcUrl, user, password)
+            .load()
     }
 
     @Bean
