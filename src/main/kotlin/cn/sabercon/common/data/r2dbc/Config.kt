6110887e831367f0f4a4c9f0a0e66815b7ebdaf9
SaberCon <894876785@qq.com>
2022-04-16 21:00:58 +0800

    :art: polish code

--- src/main/kotlin/cn/sabercon/common/data/r2dbc/Config.kt
+++ src/main/kotlin/cn/sabercon/common/data/r2dbc/Config.kt
@@ -6,7 +6,6 @@ import cn.sabercon.common.data.MTIME
 import cn.sabercon.common.util.UnsafeReflection
 import cn.sabercon.common.util.now
 import io.r2dbc.spi.ConnectionFactoryOptions
-import io.r2dbc.spi.Option
 import org.flywaydb.core.Flyway
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcProperties
 import org.springframework.context.annotation.Bean
@@ -22,12 +21,12 @@ class R2dbcConfig {
     @Bean(initMethod = "migrate")
     fun flyway(props: R2dbcProperties): Flyway {
         val options = ConnectionFactoryOptions.parse(props.url)
-        val driver = options.getValue(Option.valueOf<String>("driver")) as String
-        val host = options.getValue(Option.valueOf<String>("host")) as String
-        val port = options.getValue(Option.valueOf<Int>("port")) as Int
-        val database = options.getValue(Option.valueOf<String>("database")) as String
-        val user = options.getValue(Option.valueOf<String>("user")) as String
-        val password = options.getValue(Option.sensitiveValueOf<String>("password")) as String
+        val driver = options.getValue(ConnectionFactoryOptions.DRIVER) as String
+        val host = options.getValue(ConnectionFactoryOptions.HOST) as String
+        val port = options.getValue(ConnectionFactoryOptions.PORT) as Int
+        val database = options.getValue(ConnectionFactoryOptions.DATABASE) as String
+        val user = options.getValue(ConnectionFactoryOptions.USER) as String
+        val password = options.getValue(ConnectionFactoryOptions.PASSWORD) as String
         val jdbcUrl = "jdbc:$driver://$host:$port/$database"
 
         return Flyway.configure()
