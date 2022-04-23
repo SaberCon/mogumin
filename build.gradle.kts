1155409282735b55957f7a98d708eba1f03de568
SaberCon <894876785@qq.com>
2022-04-23 10:38:52 +0800

    Polish code

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
+    // There are some bugs with the latest version 1.20.0
     id("io.gitlab.arturbosch.detekt") version "1.19.0"
 }
 
